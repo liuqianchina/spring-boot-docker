667b7258d2d3c3b084790ff801a8aa102c4f12e9
Jovan L <327527689@qq.com>
2022-07-20 21:19:16 +0800

    Create ThreadLocalTest.java

---
+++ src/test/java/thread/ThreadLocalTest.java
@@ -0,0 +1,28 @@
+package thread;
+
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+
+public class ThreadLocalTest {
+    private List<String> messages = Lists.newArrayList();
+
+    public static final ThreadLocal<ThreadLocalTest> threadLocal = ThreadLocal.withInitial(ThreadLocalTest::new);
+
+    public static void addMessage(String message) {
+        threadLocal.get().messages.add(message);
+    }
+
+    public static List<String> clearAllMessage() {
+        final List<String> messages = threadLocal.get().messages;
+        threadLocal.remove();
+        System.out.println("The size of messages: " + threadLocal.get().messages.size());
+        return messages;
+    }
+
+    public static void main(String[] args) {
+        ThreadLocalTest.addMessage("Hello Thread Local Message 1");
+        System.out.println(threadLocal.get().messages);
+        ThreadLocalTest.clearAllMessage();
+    }
+}
